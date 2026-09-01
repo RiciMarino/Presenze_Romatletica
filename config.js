bb9a56eec664002f93bde650c404fc34e1432fd0
RiciMarino <riccardo.marino@csi-net.it>
2026-09-01 17:51:20 +0200

    Collega il sito al backend reale

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ROMATLETICA_CONFIG = {
-  backendUrl: '',
-  demoMode: true
+  backendUrl: 'https://script.google.com/macros/s/AKfycbwg5HcG3t2nKhIRp0i52UyW3lzTl47WJdgFiYdsAYBA8I71lfIw2KE6QjoRHXryHKL3/exec',
+  demoMode: false
 };
